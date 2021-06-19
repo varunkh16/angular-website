277ef982945b0e0a14a4ed4144ae49e3e998578f
Varun Kodigehalli Hanumanth Raju -X (vkodigeh - INFOSYS LIMITED at Cisco) <vkodigeh@cisco.com>
2021-06-19 13:49:03 +0530

    Pushing the initial changes done to home page

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,13 +2,37 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { ServicesComponent } from './services/services.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { TeamComponent } from './team/team.component';
+import { HomeComponent } from './home/home.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path:'', component: HomeComponent},
+  { path:'about', component: AboutComponent},
+  { path:'contact', component: ContactUsComponent},
+  { path:'services', component: ServicesComponent},
+  { path:'team', component: TeamComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    ServicesComponent,
+    ContactUsComponent,
+    TeamComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
